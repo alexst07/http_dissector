929afefba3e871aa746e9686b1741f81c5720e4e
Alex Torres <alexst07@gmail.com>
2015-06-08 22:28:54 -0300

    Loop for dissect packets

--- http-dissector.py
+++ http-dissector.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python2
 
-import pcap
+import pcapy
 import sys
 import string
 import time
 import socket
 import struct
 import optparse
+
+def parse_packet(packet):
+    print packet
 
 def main():
     p = optparse.OptionParser("usage: %prog -r <pcap-file> -d <output>")
@@ -16,6 +19,17 @@ def main():
                  dest="path_out")
 
     options, arguments = p.parse_args()
+
+    if not options.pcap_file or not options.path_out:
+        p.print_usage()
+        return
+
+    cap = pcapy.open_offline(options.pcap_file)
+
+    while(1):
+        (header, packet) = cap.next()
+        parse_packet(packet)
+
 
 if __name__ == "__main__":
     main()
