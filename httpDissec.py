37833caae3147f9c2f70b83f5b04becaa402a66e
Alex Torres <alexst07@gmail.com>
2015-06-20 15:06:17 -0300

    Change from dic to list

--- httpDissec.py
+++ httpDissec.py
@@ -6,8 +6,8 @@ from scapy.layers.http import HTTPResponse
 import sys
 
 packets = rdpcap("task07_f1.pcap")
-requests = {}
-answers = {}
+requests = []
+answers = []
 
 
 def has_http_header(packet):
@@ -17,40 +17,29 @@ for pkt in packets:
     tcp = pkt['TCP']
     # destination port must be 80
     if tcp.dport == 80 and pkt.haslayer('HTTP'):
-        ip_id = pkt[IP].id
-        requests[ip_id] = pkt
+        requests.append(pkt)
 
 for pkt in packets:
     tcp = pkt['TCP']
     # source port must be 80
     if tcp.sport == 80 and pkt.haslayer('HTTP'):
-        ip_id = pkt['IP'].id
-        answers[ip_id] = pkt
+        answers.append(pkt)
 
 
 print '=============== REQUESTS =================='
 i = 0
-for ip_id in requests:
+for req in requests:
     print 'Packet: ', i, "\n"
     i = i + 1
-    req = requests[ip_id]
     req.show()
-'''    if req.haslayer('HTTP'):
-#    if 'HTTP Request' in req:
-        httpreq = req.getlayer('HTTP')
-        print '\o/\o/\o/\o/ HTTP REQUEST \o/\o/\o/\o/\o/\o/'
-        print httpreq
-    else:
-        print '/o\ /o\ /o\ NO HTTP REQUEST'
-    	'''
+
 
 
 print '=============== ANSWERS =================='
 i = 0
-for ip_id in answers:
+for ans in answers:
     print 'Packet: ', i, "\n"
     i = i + 1
-    ans = answers[ip_id]
     if has_http_header(ans):
     	print "HAS HEADER\n"
     else:
