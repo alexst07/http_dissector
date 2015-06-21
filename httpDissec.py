fba914f2168ee9ed7427c781c3d95fa903725600
vitorlima <vitor_lp@yahoo.com>
2015-06-21 14:15:21 -0300

    Made method main that parse command line options

--- httpDissec.py
+++ httpDissec.py
@@ -5,10 +5,7 @@ from scapy.layers import http
 from scapy.layers.http import HTTPResponse
 import sys
 import os
-
-packets = rdpcap("task07_f1.pcap")
-requests = []
-answers = []
+import optparse
 
 
 def has_http_header(packet):
@@ -50,7 +47,6 @@ def extract_next_file(packets, file_name):
         return False
 
     first = packets.pop(0)
-
     # if there is no Raw length file is zero
     if "Raw" not in first:
         return False
@@ -65,24 +61,63 @@ def extract_next_file(packets, file_name):
     return True
 
 
-for pkt in packets:
-    tcp = pkt['TCP']
-    # destination port must be 80
-    if tcp.dport == 80 and pkt.haslayer('HTTP'):
-        requests.append(pkt)
+def get_http_requests (packets):
+    requests = []
+    for pkt in packets:
+        tcp = pkt['TCP']
+        # destination port must be 80
+        if tcp.dport == 80 and pkt.haslayer('HTTP'):
+            requests.append(pkt)
+    return requests
 
-for pkt in packets:
-    tcp = pkt['TCP']
-    # source port must be 80
-    if tcp.sport == 80 and pkt.haslayer('HTTP'):
-        answers.append(pkt)
 
-print '=============== REQUESTS =================='
-i = 0
-for req in requests:
-    file_name = "file_" + str(i)
-    if extract_next_file(answers, file_name):
-		printGET(req, file_name)
-    i += 1
+def get_http_responses (packets):
+    answers = []
+    for pkt in packets:
+        tcp = pkt['TCP']
+        # source port must be 80
+        if tcp.sport == 80 and pkt.haslayer('HTTP'):
+            answers.append(pkt)
+    return answers
+
+def extract_files_and_print_requests(packets, output_dir):
+    requests = get_http_requests(packets)
+    responses = get_http_responses(packets)
+
+    i = 0
+    for req in requests:
+        file_name = output_dir + "/file_" + str(i)
+        if extract_next_file(responses, file_name):
+            printGET(req, file_name)
+        i += 1
+
+
+def main():
+    p = optparse.OptionParser("usage: %prog -r <pcap-file> -d <output>")
+    p.add_option('-r', help="Filename of the pcap file is required",
+                 dest="pcap_file")
+    p.add_option('-d', help="Output path of file is required",
+                 dest="path_out")
+
+    options, arguments = p.parse_args()
+
+    if not options.pcap_file or not options.path_out:
+        p.print_usage()
+        return
+
+    output_dir = options.path_out
+    # if output directory was passed with
+    # a '/' at the end, like "out/"
+    if output_dir[-1] == '/':
+        output_dir = output_dir[0:(len(output_dir) - 1)] # remove this '/'
+
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    packets = rdpcap(options.pcap_file)
+    extract_files_and_print_requests(packets, output_dir)
+
     
+if __name__ == "__main__":
+    main()
 
